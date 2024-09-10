ce8481b83e2f9a382ca86ab80390c742b2abb8de
karelle <k.bolon.m@gmail.com>
2024-09-10 14:31:47 +0200

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This projet was coded by{" "}
+        <a href="https://www.shecodes.io/graduates/118933-karelle-bolon">
+          Karelle Bolon
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/karellemarina/my-react-weather-app"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
