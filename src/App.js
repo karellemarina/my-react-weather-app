6e0f155114c4569c9e4a49b2f72da229bc2dac5d
karelle <k.bolon.m@gmail.com>
2024-09-10 14:48:37 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This projet was coded by{" "}
-        <a href="https://www.shecodes.io/graduates/118933-karelle-bolon">
-          Karelle Bolon
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/karellemarina/my-react-weather-app"
-          target="_blank"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App</h1>
+        <footer>
+          This projet was coded by{" "}
+          <a href="https://www.shecodes.io/graduates/118933-karelle-bolon">
+            Karelle Bolon
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/karellemarina/my-react-weather-app"
+            target="_blank"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
