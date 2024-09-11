0e5acc4ce936ee2d507ada56e9505a60a2e21a95
karelle <k.bolon.m@gmail.com>
2024-09-11 18:58:16 +0200

    added forecast 1 day

---
+++ src/WeatherForecast.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="WeatherForecast-day">Thu</div>
+        <div className="col">
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
+            alt="icon"
+          ></img>
+        </div>
+        <div className="WeatherForecast-temperatures">
+          <span className="WeatherForecast-temperature-max">19</span>
+          <span className="WeatherForecast-temperature-min">10</span>
+        </div>
+      </div>
+    </div>
+  );
+}
