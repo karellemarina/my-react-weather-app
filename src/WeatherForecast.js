c1b81eb85e47120520d5087913fc50267906ec20
karelle <k.bolon.m@gmail.com>
2024-09-21 11:36:04 +0200

    added URL WeatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "8bc7430591fcdeaet361d96ab0f37o46";
+  let longitude = props.coordinates.longitude;
+  let latitude = props.coordinates.latitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="WeatherForecast-day">Thu</div>
         <div className="col">
-          <img
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
-            alt="icon"
-          ></img>
+          <div className="WeatherForecast-day">Thu</div>
+          <WeatherIcon />
         </div>
         <div className="WeatherForecast-temperatures">
           <span className="WeatherForecast-temperature-max">19</span>
